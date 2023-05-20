872da33346fd826ce404a4a44de4990e6a52fa3f
jorenchik <jorens.stekels@gmail.com>
2023-05-20 08:19:04 +0300

    feat: placeholder views
    Removed welcome view.
    
    Removed web.php comments.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+
+
+@section('content')
+	<p>Hello, layout! Here's some content for you!</p>	
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+		Index, I need some content from you.
+	</h1>
+        @yield('content')
+</body>
+
+</html>
