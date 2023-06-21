e99a48f154db6a80f46dc477c2a3d6fe0bbd68c5
jorenchik <jorens.stekels@gmail.com>
2023-06-21 16:23:09 +0300

    feat[frontend]: center internal content of the content frame

--- resources/js/Layouts/ContentFrame.jsx
+++ resources/js/Layouts/ContentFrame.jsx
@@ -1,11 +1,11 @@
 import Navbar from '@/Components/Navbar'
 
-export default function ContentFrame({children, activeNavbarElement})
+export default function ContentFrame({children, activeNavbarElement, className})
 {
 	return (
-		<div className='flex flex-col flex-grow items-center place-content-center w-full'>
+		<div className={`flex flex-col flex-grow items-center place-content-center w-full ${className}`}>
 			<Navbar className='mt-14' activeElement={activeNavbarElement} />
-			<div className='flex overflow-hidden flex-wrap flex-grow px-10 py-20 mt-14 w-full bg-white shadow-lg'>
+			<div className='flex overflow-hidden flex-col flex-grow px-10 py-20 mt-14 w-full bg-white shadow-lg'>
 				{children}
 			</div>
 		</div>
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -13,24 +13,28 @@ export default function Dashboard({ auth }) {
                 <SectionHeading>
                     Overview
                 </SectionHeading>
-                <SectionRow>
-                    <Infopanel>
-                        <span className="w-full text-5xl font-light text-slate-700"> 12:25 </span>
-                        <span className="w-full text-2xl font-light text-slate-600"> 6th June, 2023</span>
-                    </Infopanel>
-                    <VLine/>
-                    <Infopanel>
-                            <span className="w-full text-5xl font-light text-slate-700"> 8 hours 12 minutes in </span>
-                            <span className="w-full text-2xl font-light text-slate-600"> checked in at 8:13</span>
-                    </Infopanel>
-                </SectionRow>
-                <SectionRow>
-                    <Indicator number={22} changePercent={2.6} text="Appointments in June"/>
-                    <Indicator number={22} changePercent={2.6} text="Hours worked in June"/>
-                    <Indicator number={22} changePercent={2.6} text="Upcoming appointments"/>
-                    <Indicator number={22} changePercent={2.6} text="Days worked in June"/>
-                    <Indicator number={22} changePercent={2.6} text="Days worked in June"/>
-                </SectionRow>
+                <div className='flex flex-grow justify-center items-center'>
+                    <div className='flex flex-wrap'>
+                        <SectionRow>
+                            <Infopanel>
+                                <span className="w-full text-5xl font-light text-slate-700"> 12:25 </span>
+                                <span className="w-full text-2xl font-light text-slate-600"> 6th June, 2023</span>
+                            </Infopanel>
+                            <VLine/>
+                            <Infopanel>
+                                    <span className="w-full text-5xl font-light text-slate-700"> 8 hours 12 minutes in </span>
+                                    <span className="w-full text-2xl font-light text-slate-600"> checked in at 8:13</span>
+                            </Infopanel>
+                        </SectionRow>
+                        <SectionRow>
+                            <Indicator number={22} changePercent={2.6} text="Appointments in June"/>
+                            <Indicator number={22} changePercent={2.6} text="Hours worked in June"/>
+                            <Indicator number={22} changePercent={2.6} text="Upcoming appointments"/>
+                            <Indicator number={22} changePercent={2.6} text="Days worked in June"/>
+                            <Indicator number={22} changePercent={2.6} text="Days worked in June"/>
+                        </SectionRow>
+                    </div>
+                </div>
             </ContentFrame>
         </AuthenticatedLayout>
     );
