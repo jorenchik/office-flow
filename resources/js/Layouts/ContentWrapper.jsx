f219eb1b0251d27826dc43f9df963796ae182cbc
jorenchik <jorens.stekels@gmail.com>
2023-06-25 14:39:38 +0300

    feat[frontent]: removed content wrapper margin

--- resources/js/Layouts/ContentWrapper.jsx
+++ resources/js/Layouts/ContentWrapper.jsx
@@ -1,7 +1,7 @@
 export default function ContentWrapper({ children, className, ...props }) {
     return (
         <div
-            className={`flex px-[10%] mb-10 flex-col justify-center items-center ${className}`}
+            className={`flex px-[10%] flex-col justify-center items-center ${className}`}
         >
             {children}
         </div>
