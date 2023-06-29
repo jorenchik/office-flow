6016e01aa84cf8a54d8bdc94f9282e157d488e74
jorenchik <jorens.stekels@gmail.com>
2023-06-29 16:39:47 +0300

    feat: content frame style

--- resources/js/Layouts/ContentFrame.jsx
+++ resources/js/Layouts/ContentFrame.jsx
@@ -5,7 +5,7 @@ export default function ContentFrame({children, className, navbarHidden}) {
         <div className={
             `flex flex-col flex-grow items-center place-content-center w-full ${className}`
         }>
-            <div className="flex overflow-hidden flex-col flex-grow px-10 py-20 mt-14 w-full bg-white shadow-lg">
+            <div className="flex overflow-hidden flex-col flex-grow px-10 py-20 mt-14 mb-10 w-full bg-white shadow-lg">
                 {children} </div>
         </div>
     );
